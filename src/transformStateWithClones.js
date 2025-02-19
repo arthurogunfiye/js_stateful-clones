e4421f6f4cbebd8acebf16facd55593785fbc651
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-02-19 07:34:50 +0000

    Stateful clones task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,26 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state }; // Create an initial copy of the state
+  const stateHistory = [];
+
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      currentState = {}; // Reset state to an empty object
+    } else if (action.type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      currentState = { ...currentState }; // Clone before modifying
+
+      for (const key of action.keysToRemove) {
+        delete currentState[key]; // Remove specified keys
+      }
+    }
+
+    stateHistory.push({ ...currentState }); // Store a snapshot of the state
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
